2bad2cf0fa8b027ce95164bbf7db053f71a1f08b
Sandro Sorte <sandrosorte@gmail.com>
2023-01-04 02:14:03 -0300

    criação da rota Post com express, usando um exemplo simples de cadastro, testado com sucesso no Postman

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import express from 'express'
 const app = express()
 
+//indicar para o express ler body com json
+app.use(express.json())
+
+//mock
+const cadastro = [
+{id:1, nome:'Sandro', idade:36, signo:'escorpiao'},
+{id:2, nome:'Maria', idade:26, signo:'cancer'},
+{id:1, nome:'Julio', idade:36, signo:'libra'}]
+
 //Rota padrão
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
 
-app.get('/planos',(req, res)=>{
-res.send(' Listas de requisições futuras')
+app.get('/cadastro',(req, res)=>{
+res.send(cadastro)
+})
+
+app.post('/cadastro',(req, res)=>{
+  cadastro.push(req.body)
+  res.send('cadastro realizado com sucesso')
 })
 
 export default app
