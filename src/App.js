7e00ca2fb84f972cbb9c37596dafefdf91ffbffb
Sandro Sorte <sandrosorte@gmail.com>
2023-01-04 08:28:20 -0300

    Criação do método GetById e do método deleteById(Aula05)

--- src/App.js
+++ src/App.js
@@ -8,20 +8,38 @@ app.use(express.json())
 const cadastro = [
 {id:1, nome:'Sandro', idade:36, signo:'escorpiao'},
 {id:2, nome:'Maria', idade:26, signo:'cancer'},
-{id:1, nome:'Julio', idade:36, signo:'libra'}]
+{id:3, nome:'Julio', idade:36, signo:'libra'}]
+
+function buscarSelecaoPorId(id){
+  return cadastro.filter(cadastro => cadastro.id == id)
+}
+
+function buscarIndexPorId(id){
+  return cadastro.findIndex(cadastro => cadastro.id == id)
+}
 
 //Rota padrão
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Work in progress Api-Rest-NodeJs-Express')
 })
 
 app.get('/cadastro',(req, res)=>{
 res.send(cadastro)
 })
 
+app.get('/cadastro/:id',(req, res) => {
+  res.json(buscarSelecaoPorId(req.params.id))
+})
+
 app.post('/cadastro',(req, res)=>{
   cadastro.push(req.body)
   res.send('cadastro realizado com sucesso')
+})
+
+app.delete('/cadastro/:id', (req, res)=>{
+  let index = buscarIndexPorId(req.params.id)
+  cadastro.splice(index, 1)
+  res.send(`cadastro com id ${req.params.id} removido com sucesso`)
 })
 
 export default app
