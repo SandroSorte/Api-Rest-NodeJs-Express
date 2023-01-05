ec2c3b6b65f7070c7eeacb323b216e4c7fb87c20
Sandro Sorte <sandrosorte@gmail.com>
2023-01-05 10:54:01 -0300

    inclusão da pasta infra, que estabelece a conexao com o banco de dados MySql

--- Server.js
+++ Server.js
@@ -1,7 +1,15 @@
 import app from "./src/App.js"
+import Conexao from './Infra/Conexao.js'
 
 const PORT = 3000
 
-app.listen(PORT, () => {
+Conexao.connect((erro)=>{
+  if(erro){
+    console.log(erro)
+  }else{
+    console.log('conexão realizada com sucesso')
+  }
+  app.listen(PORT, () => {
     console.log(`Servidor rodando no endereço http://localhost:${PORT}`)
   })
+})
